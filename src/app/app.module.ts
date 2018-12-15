801dd8fc057e8e0131f1d3b495ea6dcf501c67e5
Jeffery Cosby <jcoz1968@hotmail.com>
2018-12-15 15:12:59 -0600

    angular forms

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 
 import { AppComponent } from './app.component';
@@ -15,6 +16,7 @@ import { NewWeightEntryComponent } from './new-weight-entry/new-weight-entry.com
   imports: [
     CommonModule,
     BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
