000292aacae2fcfc50b6edc49ee3d0630b4e3dbf
Jeffery Cosby <jcoz1968@hotmail.com>
2018-12-15 14:41:30 -0600

    inputs

--- src/app/new-weight-entry/new-weight-entry.component.ts
+++ src/app/new-weight-entry/new-weight-entry.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'hm-new-weight-entry',
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NewWeightEntryComponent implements OnInit {
 
+  @Input() showBodyFat: boolean;
   constructor() { }
 
   ngOnInit() {
