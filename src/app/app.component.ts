3f8f823285e84e8c53b58c0d0522e1abf4a62bf4
Jeffery Cosby <jcoz1968@hotmail.com>
2018-12-15 13:41:20 -0600

    added service, model binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 
+import { WeightEntriesService } from './weight-entries.service';
+
 @Component({
   selector: 'hm-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
-  constructor() {}
+  constructor(public entriesService: WeightEntriesService) {}
 
   ngOnInit() {}
 }
