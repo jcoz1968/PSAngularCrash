af6579b6e5c5cc9c43fac654bba7c329b4946eb0
Jeffery Cosby <jcoz1968@hotmail.com>
2018-12-15 16:15:51 -0600

    login component, routing, login service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,22 +3,30 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { NewWeightEntryComponent } from './new-weight-entry/new-weight-entry.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     NewWeightEntryComponent,
+    LoginComponent,
   ],
   imports: [
     CommonModule,
     BrowserModule,
     FormsModule,
-    HttpClientModule
+    HttpClientModule,
+    RouterModule.forRoot([
+      { path: '', redirectTo: '/home', pathMatch: 'full' },
+      { path: 'home', component: HomeComponent },
+      { path: 'login', component: LoginComponent }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
